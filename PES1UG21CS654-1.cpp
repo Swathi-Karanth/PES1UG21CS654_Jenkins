335a00e2e84a257d1e1e0a4505f0b5f46be04356
Swathi Karanth <107118761+Swathi-Karanth@users.noreply.github.com>
2024-03-06 13:05:18 +0530

    Create PES1UG21CS654-1.cpp, task5

---
+++ PES1UG21CS654-1.cpp
@@ -0,0 +1,10 @@
+//File: PES1UG21CS654-1.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Name: Swathi Karanth\n";
+  cout << "SRN: PES1UG21CS654\n";
+  cout << "\n\nStatus: Task 5 completed. Jenkins hello world pipeline created!\n";
+  return 0;
+}
